e7bdc13de4d671f5390b2205353d28856a845335
afuawonders <afuawonders@gmail.com>
2023-08-29 22:55:46 +0000

    sorting algorithym

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,27 +1,25 @@
 #include "sort.h"
 /**
-*bubble_sort - sorts an array of int in ascending orde
-*@array: array of numbers
-*@size: Size of array
-*/
+ * bubble_sort - sort array lements from min to max value
+ * @array: array
+ * @size: array size
+ */
 void bubble_sort(int *array, size_t size)
 {
-size_t j, i;
-int num;
 
-if (array == NULL  || size < 2)
-return;
-for (i = 0; i < size - 1; i++)
-{
-for (j = 0; j < size - i - 1; j++)
-{
-if (array[j] > array[j + 1])
-{
-num = array[j];
-array[j] = array[j + 1];
-array[j + 1] = num;
-print_array(array, size);
-}
-}
-}
+	size_t firstIndex, innerIndex, tmp = 0;
+
+	if (size < 2)
+		return;
+	for (firstIndex = 0; firstIndex < size; firstIndex++)
+		for (innerIndex = 0; innerIndex < size; innerIndex++)
+		{
+			if (array[innerIndex] > array[innerIndex + 1] && array[innerIndex + 1])
+			{
+			tmp = array[innerIndex];
+			array[innerIndex] = array[innerIndex + 1];
+			array[innerIndex + 1] = tmp;
+			print_array(array, size);
+			}
+		}
 }
--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -1,47 +1,46 @@
 #include "sort.h"
 /**
-* insertion_sort_list - sort a list
-* @list: UnSorted Linked list
-*/
-
+ * insertion_sort_list - function that sorts a doubly linked list
+ * of integers in ascending order using the Insertion sort algorithm
+ * @list: Dobule linked list to sort
+ */
 void insertion_sort_list(listint_t **list)
 {
-listint_t *node;
+	listint_t *node;
 
-if (list == NULL || (*list)->next == NULL)
-return;
-node = (*list)->next;
-while (node)
-{
-while ((node->prev) && (node->prev->n > node->n))
-{
-node = swap_node(node, list);
-print_list(*list);
-}
-node = node->next;
-}
+	if (list == NULL || (*list)->next == NULL)
+		return;
+	node = (*list)->next;
+	while (node)
+	{
+		while ((node->prev) && (node->prev->n > node->n))
+		{
+			node = swapNode(node, list);
+			displayList(*list);
+		}
+		node = node->next;
+	}
 }
 /**
-*swap_node - swap a node helper
-*@node: node
-*@list: node list
-*Return: return pointer of current node
-*/
-
-listint_t *swap_node(listint_t *node, listint_t **list)
+ *swapNode - swap a node for his previous one
+ *@node: node
+ *@list: node list
+ *Return: return a pointer to a node which was enter it
+ */
+listint_t *swapNode(listint_t *node, listint_t **list)
 {
-listint_t *back = node->prev, *current = node;
-/*NULL, 19, 48, 9, 71, 13, NULL*/
+	listint_t *backNode = node->prev, *currentNode = node;
+	/*NULL, 19, 48, 9, 71, 13, NULL*/
 
-back->next = current->next;
-if (current->next)
-current->next->prev = back;
-current->next = back;
-current->prev = back->prev;
-back->prev = current;
-if (current->prev)
-current->prev->next = current;
-else
-*list = current;
-return (current);
+	backNode->next = currentNode->next;
+	if (currentNode->next)
+		currentNode->next->prev = backNode;
+	currentNode->next = backNode;
+	currentNode->prev = backNode->prev;
+	backNode->prev = currentNode;
+	if (currentNode->prev)
+		currentNode->prev->next = currentNode;
+	else
+		*list = currentNode;
+	return (currentNode);
 }
